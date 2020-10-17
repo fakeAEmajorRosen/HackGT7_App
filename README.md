10ea8f5f0c7b3a75a24a0f2802dea30040964fc2
fakeAEmajorRosen <tyu304@gatech.edu>
2020-10-17 16:54:49 -0400

    shit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HackGT7_App
 Hi!
+Mock-up: https://www.figma.com/file/I15veOwn8BSn90tfz0KgyS/HackGT7?node-id=26%3A18
---
+++ hackgt_app/android/app/src/main/kotlin/com/example/hackgt_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hackgt_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
